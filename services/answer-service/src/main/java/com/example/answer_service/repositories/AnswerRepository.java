e150b222574d415ae1f6c1dc79799b97e7ed222f
Seif Hossam Sabra <110059706+seifhossam2002@users.noreply.github.com>
2025-04-28 21:55:04 +0300

    Updated AnswerRepository

--- services/answer-service/src/main/java/com/example/answer_service/repositories/AnswerRepository.java
+++ services/answer-service/src/main/java/com/example/answer_service/repositories/AnswerRepository.java
@@ -4,6 +4,8 @@ import com.example.answer_service.model.Answer;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
 public interface AnswerRepository extends MongoRepository<Answer, UUID> {
 }
