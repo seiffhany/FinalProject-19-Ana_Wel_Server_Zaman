adde967a7cdf8be6873eecde974532d03ceb91cc
Seif Hany <seif.naguib@gmail.com>
2025-05-10 12:38:34 +0300

    Setup Files for Builder and Strategy

---
+++ services/question-service/src/main/java/com/example/question_service/model/builder/QuestionBuilder.java
@@ -0,0 +1,5 @@
+package com.example.question_service.model.builder;
+
+public class QuestionBuilder {
+    
+}
---
+++ services/question-service/src/main/java/com/example/question_service/service/filter/QuestionFilterStrategy.java
@@ -0,0 +1,5 @@
+package com.example.question_service.service.filter;
+
+public class QuestionFilterStrategy {
+    
+}
