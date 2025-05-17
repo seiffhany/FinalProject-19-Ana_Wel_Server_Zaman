36a58fc17bbf16a1f7d3871370c93dbc9a072165
Maryse  Samuel <118538079+maryseSamuel@users.noreply.github.com>
2025-05-17 19:25:29 +0300

    filter strategy DONE

--- services/question-service/src/main/java/com/example/question_service/service/filter/TagFilterStrategy.java
+++ services/question-service/src/main/java/com/example/question_service/service/filter/TagFilterStrategy.java
@@ -1,5 +1,19 @@
 package com.example.question_service.service.filter;
 
-public class TagFilterStrategy {
+import com.example.question_service.model.Question;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+public class TagFilterStrategy implements QuestionFilterStrategy{
+
+    @Override
+    public List<Question> filter(List<Question> questions, String tag) {
+        return questions.stream()
+                .filter(q -> q.getTags() != null && q.getTags().contains(tag))
+                .collect(Collectors.toList());
+    }
+
 
 }
